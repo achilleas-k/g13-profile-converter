63a3e0ea64149fadd975b5f1623a3b8b33aa687c
Achilleas <achilleas@morgana>
2013-07-09 05:37:59 +0300

    Reads macros and builds dictionary

---
+++ wintonix.py
@@ -0,0 +1,33 @@
+import xml.etree.ElementTree as ET
+
+filename = sys.argv[1] # TODO: check arguments
+
+tree = ET.parse(filename)
+root = tree.getroot()
+
+profile = root[0]
+profile_name = profile.attrib['name']
+profile_id = profile.attrib['guid']
+for pchild in profile:
+    if "macros" in pchild.tag:
+        macros_elems = pchild
+    elif "assignments" in pchild.tag:
+        assignments_elems = pchild
+    elif "backlight" in pchild.tags:
+        backlight_elems = pchild
+
+macros = []
+for macro_el in macros_elems:
+    newmacro = {}
+    newmacro['name'] = macro_el.get('name')
+    newmacro['guid'] = macro_el.get('guid')
+    newmacro['key'] = macro_el.getchildren()[0].getchildren()[0].get('value')
+                        # ^^^ ugly, but works
+
+
+
+
+
+
+
+
